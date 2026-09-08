bdaf28bbd8cafb46a48e9819c3b8973ef228ebc5
Yuhang <donascimentohannah49@gmail.com>
2026-09-08 18:35:15 +0800

    Add files via upload

---
+++ utils/path_tool.py
@@ -0,0 +1,19 @@
+"""
+为整个工程提供统一的绝对路径
+"""
+import os
+
+def get_project_root()->str:
+    # 当前文件的绝对路径
+    current_file = os.path.abspath(__file__)
+    # 获取工程的根目录
+    current_dir = os.path.dirname(current_file)
+    # 获取工程根目录
+    project_root = os.path.dirname(current_dir)
+    return project_root
+
+def get_abs_path(relative_path:str)->str:
+    # 传入相对路径，返回绝对路径
+    project_root = get_project_root()
+    return os.path.join(project_root,relative_path)
+
